99d2b1135ba1bd2445618f1e59845e42c4084201
mathius johnson <johnson_m@qwasar.io>
2023-11-12 05:01:31 +0000

    chore: scaffold function

---
+++ my_readline.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int READLINE_READ_SIZE = 512;
+
+char *my_readline(int fd) {
+    
+}
+
+int main(int ac, char **av) {
+  char *str = NULL;
+
+  int fd = open("./file.txt", O_RDONLY);
+  while ((str = my_readline(fd)) != NULL)
+  {
+      printf("%s\n", str);
+      free(str);
+  }
+  close(fd);
+  //
+  //  Yes it's also working with stdin :-)
+  //  printf("%s", my_readline(0));
+  //
+
+  return 0;
+}
