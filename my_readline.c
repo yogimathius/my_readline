8090f3b41c340e3bcfa79ff870ea2d64c47ae925
mathius johnson <johnson_m@qwasar.io>
2023-11-30 06:55:27 +0000

    feat: pass first test

--- my_readline.c
+++ my_readline.c
@@ -19,14 +19,18 @@ void init_my_readline() {
 }
 
 char *my_readline(int fd) {
+    // printf("=============\n");
     int buffer_start = readline_buffer[0];
     int buffer_end = readline_buffer[1];
-
+    // printf("buffer_start: %d\n", buffer_start);
+    // printf("buffer_end: %d\n", buffer_end);
     if (buffer_start == buffer_end) {
         buffer_end = read(fd, readline_buffer + 2, READLINE_READ_SIZE) + 2;
         buffer_start = 2;
+        // printf("buffer_end: %d\n", buffer_end);
 
         if (buffer_end <= 2) {
+            // printf("checks out\n");
             free(readline_buffer);
             readline_buffer = NULL;
             return NULL;
@@ -39,21 +43,28 @@ char *my_readline(int fd) {
     int newline_pos = -1;
     for (int i = buffer_start; i < buffer_end; i++) {
         if (readline_buffer[i] == '\n') {
+            // printf("setting new line: %d\n", i);
             newline_pos = i;
             break;
         }
     }
-
     int line_length = (newline_pos != -1) ? newline_pos - buffer_start : buffer_end - buffer_start;
+    // printf("line_length: %d\n", line_length);
+    // printf("newline_pos: %d\n", newline_pos);
     char *line = malloc((line_length + 1) * sizeof(char));
     if (line == NULL) {
         return NULL;
     }
 
+    // printf("buffer_start: %d, buffer_end: %d\n", buffer_end, buffer_start);
     memcpy(line, &readline_buffer[buffer_start], line_length);
-    line[line_length] = '\0';
+    // if (newline_pos != -1) {
+        line[line_length] = '\0';
+    // }
+    // printf("readline_buffer: %s\n", readline_buffer);
 
     buffer_start = (newline_pos != -1) ? newline_pos + 1 : buffer_end;
+    // printf("buffer_start updated: %d, buffer_end: %d\n", buffer_end, buffer_start);
 
     readline_buffer[0] = buffer_start;
     readline_buffer[1] = buffer_end;
@@ -61,14 +72,22 @@ char *my_readline(int fd) {
     return line;
 }
 
-int main(int ac, char **av) {
-    char *str = NULL;
-    init_my_readline();
-    int fd = open(av[1], O_RDONLY);
-    while ((str = my_readline(fd)) != NULL) {
-        printf("%s\n", str);
-        free(str);
-    }
-    close(fd);
-    return 0;
-}
+
+  
+// int main(int ac, char **av) {
+
+//   if (ac == 3) {
+//     int fd = open(av[1], O_RDONLY);
+
+//     READLINE_READ_SIZE = atoi(av[2]);
+
+//     char *line = NULL;
+//     init_my_readline();
+
+//     while ((line = my_readline(fd))) {
+//       printf("%s\n", line);
+//       free(line);
+//     }
+//     close(fd);
+//   }
+// }
