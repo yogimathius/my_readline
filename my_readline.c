94629515d607df03d79856218da7d17e5be0a06a
mathius johnson <johnson_m@qwasar.io>
2023-12-09 08:42:03 +0000

    wip

--- my_readline.c
+++ my_readline.c
@@ -9,85 +9,131 @@ int READLINE_READ_SIZE = 512;
 char *readline_buffer = NULL;
 
 void init_my_readline() {
-    readline_buffer = malloc((READLINE_READ_SIZE + 2) * sizeof(char));
+    readline_buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
     if (readline_buffer == NULL) {
         perror("Failed to allocate memory for readline buffer");
         exit(EXIT_FAILURE);
     }
-    readline_buffer[0] = 0; // buffer_start
-    readline_buffer[1] = 0; // buffer_end
+    readline_buffer[0] = '\0';
+}
+
+void append_to_line(char **line, const char *str) {
+    size_t new_length = strlen(str);
+    if (*line == NULL) {
+        *line = strdup(str);
+        if (*line == NULL) {
+            perror("Failed to allocate memory for line");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        size_t current_length = strlen(*line);
+        *line = realloc(*line, current_length + new_length + 1);
+        if (*line == NULL) {
+            perror("Failed to reallocate memory for line");
+            exit(EXIT_FAILURE);
+        }
+        strcat(*line, str);
+    }
+}
+
+void append_to_readline_buffer(const char *str) {
+    size_t current_length = (readline_buffer != NULL) ? strlen(readline_buffer) : 0;
+    size_t new_length = strlen(str);
+
+    readline_buffer = realloc(readline_buffer, current_length + new_length + 1);
+    if (readline_buffer == NULL) {
+        perror("Failed to reallocate memory for readline_buffer");
+        exit(EXIT_FAILURE);
+    }
+
+    if (strncat(readline_buffer, str, new_length) == NULL) {
+        perror("Failed to concatenate to readline_buffer");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int has_newline(char *buffer){
+  for (int i = 0; buffer[i] != '\0'; i ++) {
+    if (buffer[i] == '\n') {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void copy_sides(char *right, char *left, char *temp_buffer, int newline_pos) {
+  strncpy(left, temp_buffer, newline_pos);
+  left[newline_pos] = '\0';
+
+  strncpy(right, &temp_buffer[newline_pos + 2], READLINE_READ_SIZE - newline_pos);
+  right[READLINE_READ_SIZE - newline_pos - 1] = '\0';
 }
 
 char *my_readline(int fd) {
-    // printf("=============\n");
-    int buffer_start = readline_buffer[0];
-    int buffer_end = readline_buffer[1];
-    // printf("buffer_start: %d\n", buffer_start);
-    // printf("buffer_end: %d\n", buffer_end);
-    if (buffer_start == buffer_end) {
-        buffer_end = read(fd, readline_buffer + 2, READLINE_READ_SIZE) + 2;
-        buffer_start = 2;
-        // printf("buffer_end: %d\n", buffer_end);
+  char *line = NULL;
+  char *temp_buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
 
-        if (buffer_end <= 2) {
-            // printf("checks out\n");
-            free(readline_buffer);
-            readline_buffer = NULL;
-            return NULL;
+  int newline_pos = -1;
+  if (readline_buffer[0] != '\0') {
+    // printf("adding readline to line %s\n", readline_buffer);
+    append_to_line(&line, readline_buffer);
+    free(readline_buffer);
+    init_my_readline();
+  }
+  ssize_t bytesRead;
+  while ((bytesRead = read(fd, temp_buffer, READLINE_READ_SIZE)) > 0) {
+    temp_buffer[bytesRead] = '\0';
+
+
+    if ((newline_pos = has_newline(temp_buffer)) != -1) {
+      if (newline_pos == 0) {
+        printf("newline at beginning %s\n", temp_buffer);
+        memmove(temp_buffer, temp_buffer+1, strlen(temp_buffer));
+        append_to_readline_buffer(temp_buffer);
+      } else if (newline_pos == READLINE_READ_SIZE) {
+        printf("newline at end %s\n", temp_buffer);
+      } else {
+        printf("in the middle: %s\n", temp_buffer);
+        char *right = malloc((READLINE_READ_SIZE - newline_pos) * sizeof(char));
+        char *left = malloc((newline_pos + 1) * sizeof(char));
+
+        if (left == NULL || right == NULL) {
+            perror("Failed to allocate memory for left/right");
+            exit(EXIT_FAILURE);
         }
+        copy_sides(right, left, temp_buffer, newline_pos);
 
-        readline_buffer[0] = buffer_start;
-        readline_buffer[1] = buffer_end;
+        append_to_line(&line, left);
+        append_to_readline_buffer(right);
+        free(left);
+        free(right);
+        free(temp_buffer); 
+
+        return line;
+      }
+    } else {
+      append_to_line(&line, temp_buffer);
     }
+  }
+  free(temp_buffer); 
 
-    int newline_pos = -1;
-    for (int i = buffer_start; i < buffer_end; i++) {
-        if (readline_buffer[i] == '\n') {
-            // printf("setting new line: %d\n", i);
-            newline_pos = i;
-            break;
-        }
-    }
-    int line_length = (newline_pos != -1) ? newline_pos - buffer_start : buffer_end - buffer_start;
-    // printf("line_length: %d\n", line_length);
-    // printf("newline_pos: %d\n", newline_pos);
-    char *line = malloc((line_length + 1) * sizeof(char));
-    if (line == NULL) {
-        return NULL;
-    }
-
-    // printf("buffer_start: %d, buffer_end: %d\n", buffer_end, buffer_start);
-    memcpy(line, &readline_buffer[buffer_start], line_length);
-    // if (newline_pos != -1) {
-        line[line_length] = '\0';
-    // }
-    // printf("readline_buffer: %s\n", readline_buffer);
-
-    buffer_start = (newline_pos != -1) ? newline_pos + 1 : buffer_end;
-    // printf("buffer_start updated: %d, buffer_end: %d\n", buffer_end, buffer_start);
-
-    readline_buffer[0] = buffer_start;
-    readline_buffer[1] = buffer_end;
-
-    return line;
+  return line;
 }
 
+int main(int ac, char **av) {
 
-  
-// int main(int ac, char **av) {
+  if (ac == 3) {
+    int fd = open(av[1], O_RDONLY);
 
-//   if (ac == 3) {
-//     int fd = open(av[1], O_RDONLY);
+    READLINE_READ_SIZE = atoi(av[2]);
 
-//     READLINE_READ_SIZE = atoi(av[2]);
+    char *line = NULL;
+    init_my_readline();
 
-//     char *line = NULL;
-//     init_my_readline();
-
-//     while ((line = my_readline(fd))) {
-//       printf("%s\n", line);
-//       free(line);
-//     }
-//     close(fd);
-//   }
-// }
+    while ((line = my_readline(fd))) {
+      printf("from main: %s\n", line);
+      free(line);
+    }
+    close(fd);
+  }
+}
