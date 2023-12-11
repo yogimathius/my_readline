70f5575d8cad553f84927ac80f10891047005452
mathius johnson <johnson_m@qwasar.io>
2023-12-11 17:53:04 +0000

    wip

--- my_readline.c
+++ my_readline.c
@@ -39,7 +39,6 @@ void append_to_line(char **line, const char *str) {
 void append_to_readline_buffer(const char *str) {
     size_t current_length = (readline_buffer != NULL) ? strlen(readline_buffer) : 0;
     size_t new_length = strlen(str);
-
     readline_buffer = realloc(readline_buffer, current_length + new_length + 1);
     if (readline_buffer == NULL) {
         perror("Failed to reallocate memory for readline_buffer");
@@ -65,20 +64,86 @@ void copy_sides(char *right, char *left, char *temp_buffer, int newline_pos) {
   strncpy(left, temp_buffer, newline_pos);
   left[newline_pos] = '\0';
 
-  strncpy(right, &temp_buffer[newline_pos + 2], READLINE_READ_SIZE - newline_pos);
+  strncpy(right, &temp_buffer[newline_pos + 1], READLINE_READ_SIZE - newline_pos);
   right[READLINE_READ_SIZE - newline_pos - 1] = '\0';
 }
+
+// void *check_for_newlines(char *temp_buffer, int newline_pos, char **line) {
+//     if ((newline_pos = has_newline(temp_buffer)) != -1) {
+//       if (newline_pos == 0) {
+//         printf("newline at beginning %s\n", temp_buffer);
+//         memmove(temp_buffer, temp_buffer+1, strlen(temp_buffer));
+//         append_to_readline_buffer(temp_buffer);
+//       } else if (newline_pos == READLINE_READ_SIZE) {
+//         printf("newline at end %s\n", temp_buffer);
+//       } else {
+//         printf("in the middle: %s\n", temp_buffer);
+//         char *right = malloc((READLINE_READ_SIZE - newline_pos) * sizeof(char));
+//         char *left = malloc((newline_pos + 1) * sizeof(char));
+
+//         if (left == NULL || right == NULL) {
+//             perror("Failed to allocate memory for left/right");
+//             exit(EXIT_FAILURE);
+//         }
+//         copy_sides(right, left, temp_buffer, newline_pos);
+
+//         append_to_line(line, left);
+//         append_to_readline_buffer(right);
+//         free(left);
+//         free(right);
+//         free(temp_buffer); 
+//       }
+//     } else {
+//       append_to_line(line, temp_buffer);
+//     }
+// }
 
 char *my_readline(int fd) {
   char *line = NULL;
   char *temp_buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
-
   int newline_pos = -1;
   if (readline_buffer[0] != '\0') {
-    // printf("adding readline to line %s\n", readline_buffer);
-    append_to_line(&line, readline_buffer);
+      if ((newline_pos = has_newline(readline_buffer)) != -1) {
+        char *right = malloc((READLINE_READ_SIZE - newline_pos) * sizeof(char));
+        char *left = malloc((newline_pos + 1) * sizeof(char));
+
+        if (left == NULL || right == NULL) {
+            perror("Failed to allocate memory for left/right");
+            exit(EXIT_FAILURE);
+        }      
+        copy_sides(right, left, readline_buffer, newline_pos);
+        if (newline_pos == 0) {
+            // append_to_line(&line, readline_buffer);
+            strncpy(right, &readline_buffer[newline_pos + 1], READLINE_READ_SIZE - newline_pos);
+            right[READLINE_READ_SIZE - newline_pos - 1] = '\0';
+            free(readline_buffer);
+            init_my_readline();
+
+            append_to_readline_buffer(right);
+        } else if (newline_pos == READLINE_READ_SIZE) {
+            printf("newline at end %s\n", readline_buffer);
+        } else {
+            printf("in the middle: %s\n", readline_buffer);
+            append_to_line(&line, left);
+            free(readline_buffer);
+            init_my_readline();
+
+            append_to_readline_buffer(right);
+
+            free(temp_buffer);
+            return line;
+        }
+        free(left);
+        free(right);
+      } else {
+        append_to_line(&line, readline_buffer);
+      }
     free(readline_buffer);
+    free(temp_buffer);
     init_my_readline();
+    printf("returning\n");
+    return line;
+
   }
   ssize_t bytesRead;
   while ((bytesRead = read(fd, temp_buffer, READLINE_READ_SIZE)) > 0) {
@@ -89,11 +154,12 @@ char *my_readline(int fd) {
       if (newline_pos == 0) {
         printf("newline at beginning %s\n", temp_buffer);
         memmove(temp_buffer, temp_buffer+1, strlen(temp_buffer));
+        printf("removed newling: %s\n", temp_buffer);
         append_to_readline_buffer(temp_buffer);
       } else if (newline_pos == READLINE_READ_SIZE) {
         printf("newline at end %s\n", temp_buffer);
       } else {
-        printf("in the middle: %s\n", temp_buffer);
+        // printf("in the middle: %s\n", temp_buffer);
         char *right = malloc((READLINE_READ_SIZE - newline_pos) * sizeof(char));
         char *left = malloc((newline_pos + 1) * sizeof(char));
 
